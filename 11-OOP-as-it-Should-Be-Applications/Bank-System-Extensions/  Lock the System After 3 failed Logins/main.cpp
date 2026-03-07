969ed43a04c64aff1562a24ff440cd1164ab9381
Ali Rifai <ammmrrr18@gmail.com>
2026-03-07 17:11:04 +0200

    Create main.cpp

---
+++ 11-OOP-as-it-Should-Be-Applications/Bank-System-Extensions/  Lock the System After 3 failed Logins/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "clsLoginScreen.h"
+
+
+int main()
+
+{
+
+    while (true)
+    {
+
+
+        if (!clsLoginScreen::ShowLoginScreen())
+        {
+            break;
+        }
+    }
+
+   
+    return 0;
+}
