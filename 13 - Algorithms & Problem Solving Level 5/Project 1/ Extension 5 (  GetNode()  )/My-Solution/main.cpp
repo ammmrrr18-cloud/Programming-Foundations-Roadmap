a5a01abc139e62a8aa108ade254fe2cdf77ea6f5
Ali Rifai <ammmrrr18@gmail.com>
2026-05-04 00:02:06 +0300

    Create main.cpp

---
+++ 13 - Algorithms & Problem Solving Level 5/Project 1/ Extension 5 (  GetNode()  )/My-Solution/main.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+#include "clsDblLinkedList.h"
+
+using namespace std;
+
+
+int main()
+{
+
+    clsDblLinkedList <int> MydblLinkedList;
+
+ 
+    MydblLinkedList.InsertAtBeginning(5);
+    MydblLinkedList.InsertAtBeginning(4);
+    MydblLinkedList.InsertAtBeginning(3);
+    MydblLinkedList.InsertAtBeginning(2);
+    MydblLinkedList.InsertAtBeginning(1);
+
+    cout << "\nLinked List Contenet:\n";
+    MydblLinkedList.PrintList();
+
+    
+
+    clsDblLinkedList<int>::Node* N;
+
+    N = MydblLinkedList.GetNode(1);
+   
+    cout << "\nValue is : " << N->value << endl;
+    
+
+
+    system("pause>0");
+
+}
