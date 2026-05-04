6ce4a7c0aca9f835d5c900e09baccbf938986514
Ali Rifai <ammmrrr18@gmail.com>
2026-05-04 18:30:03 +0300

    Create main.cpp

---
+++ 13 - Algorithms & Problem Solving Level 5/Project 1/ Extension 6 (  clsDblLinkedList . GetItem( int index )   )/main.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+#include "clsDblLinkedList.h"
+
+using namespace std;
+
+
+int main()
+{
+
+    clsDblLinkedList <int> MydblLinkedList;
+
+ 
+    MydblLinkedList.InsertAtBeginning(5);
+    MydblLinkedList.InsertAtBeginning(4);
+    MydblLinkedList.InsertAtBeginning(3);
+    MydblLinkedList.InsertAtBeginning(2);
+    MydblLinkedList.InsertAtBeginning(1);
+
+    cout << "\nLinked List Contenet:\n";
+    MydblLinkedList.PrintList();
+
+    
+
+    
+
+    cout << "\n\nItem (2) Vlaue is : " << MydblLinkedList.GetItem(2);
+    
+
+ 
+
+
+
+    system("pause>0");
+
+}
