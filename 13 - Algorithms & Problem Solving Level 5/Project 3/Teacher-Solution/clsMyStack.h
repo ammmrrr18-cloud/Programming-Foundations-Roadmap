6551500c21fe969954838c85a651d9c29f78edcb
Ali Rifai <ammmrrr18@gmail.com>
2026-05-07 00:41:29 +0300

    Update clsMyStack.h

--- 13 - Algorithms & Problem Solving Level 5/Project 3/Teacher-Solution/clsMyStack.h
+++ 13 - Algorithms & Problem Solving Level 5/Project 3/Teacher-Solution/clsMyStack.h
@@ -26,3 +26,40 @@ public:
 
 };
 
+
+
+
+// use (this) is better
+
+#pragma once
+#include "clsMyQueue.h"
+
+template<typename T>
+class clsMyStack :public clsMyQueue<T>
+{
+public:
+
+	void push(T Item)
+	{
+		//clsMyQueue <T>::_MyList.InsertAtBeginning(Item);
+		this->_MyList.InsertAtBeginning(Item);
+	}
+
+	T Top()
+	{
+		//return clsMyQueue <T>::front();
+		return	this->front();
+
+	}
+
+	T Bottom()
+	{
+		//return clsMyQueue <T>::back();,
+		return this->back();
+
+	}
+
+
+};
+
+
